9f235f2724102f0f281dd4616c0974af0f2aa0ca
unknown <jorra04@my.yorku.ca>
2022-01-06 23:49:01 -0500

    Added updated functionality with uuid.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const admin = require("firebase-admin");
 const express = require("express");
 const app = express();
-var serviceAccount = require("./permissions.js");
+const { v4: uuidv4 } = require('uuid');
+const serviceAccount = require("./permissions.js");
 app.use(express.json());
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -12,12 +13,13 @@ app.use(cors({ origin: true }));
 const db = admin.firestore();
 //Routes
 app.get("/", async (req, res) => {
-  res.send("Hello, World!");
+  res.send("Welcome to my Submission for the 2022 Shopify Backend Internship Challenge!");
 });
 
 //Create
 app.post("/create", async (req, res) => {
-  const inventoryItems = db.collection("Inventory").doc();
+  const generatedUUID = uuidv4();
+  const inventoryItems = db.collection("Inventory").doc(generatedUUID);
   const data = req.body;
 
   await inventoryItems.set({
@@ -28,12 +30,53 @@ app.post("/create", async (req, res) => {
     unitPrice : data.unitPrice
   });
 
-  res.send("Added");
+  res.send("Inventory Item Successfully Added.");
 
 });
 //Read
+/*
+Here we are getting all items.
+*/
+app.get("/items", async (req,res) => {
+  const inventoryItems = db.collection("Inventory");
+  const inventoryDocs = await inventoryItems.get();
+  let inventoryItemsArray = [];
+  inventoryDocs.forEach(doc => {
+    inventoryItemsArray.push(doc.data());
+  })
+
+  res.send(inventoryItemsArray);
+});
+
+/*
+Here we are getting an item with a specific ID.
+*/
+app.get("/items/:itemID", async (req,res) => {
+  const inventoryItems = db.collection("Inventory");
+  const inventoryDocs = await inventoryItems.where("ID", "==", req.params.itemID).get();
+  let inventoryItemsArray = [];
+  inventoryDocs.forEach(doc => {
+    inventoryItemsArray.push(doc.data());
+  })
+
+  res.send(inventoryItemsArray);
+
+
+});
 
 //Update
+
+app.put("/items/updateItem/:itemID", async (req,res) => {
+
+  // const data = req.body;
+  // const inventoryItems = db.collection("Inventory");
+  // const inventoryDocs = await inventoryItems.get();
+  // inventoryDocs.forEach(doc => {
+  //   if(doc.)
+  // })
+  
+
+});
 
 //Delete
 
